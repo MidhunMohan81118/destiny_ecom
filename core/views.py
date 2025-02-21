9a67045dfdc74de992d982ee5560a9b195e188c5
Midhun <midhunmohan81118@gmail.com>
2025-02-21 15:15:59 +0530

    second commit

--- core/views.py
+++ core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def home(request):
-    return HttpResponse("<h1> Home Page </h1>")
+    return render(request,'core/index.html')
+
+
+@login_required(login_url="/admin")
+def login(request):
+    return render(request,'core/authentication.html')
